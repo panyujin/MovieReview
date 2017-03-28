7351f9ef70e51dc80592587329f52804bb4dee25
panyujin <panyujinyx@gmail.com>
2017-03-28 23:14:06 +0800

    join & quit movie action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -42,6 +42,32 @@ class MoviesController < ApplicationController
      redirect_to movies_path
    end
 
+   def join
+     @movie = Movie.find(params[:id])
+
+     if !current_user.is_member_of?(@movie)
+       current_user.join!(@movie)
+       flash[:notice] = "你已经收藏成功!"
+     else
+       flash[:warning] = "你已经收藏过了!"
+     end
+
+     redirect_to movie_path(@movie)
+   end
+
+   def quit
+     @movie = Movie.find(params[:id])
+
+     if current_user.is_member_of?(@movie)
+       current_user.quit!(@movie)
+       flash[:alert] = "你已经取消收藏!"
+     else
+       flash[:warning] = "你没有收藏,怎么取消 XD"
+     end
+
+     redirect_to movie_path(@movie)
+   end
+
    private
 
    def find_movie_and_check_permission
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :movies do
+    member do
+      post :join
+      post :quit
+    end
+
     resources :reviews
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#index'
 end
