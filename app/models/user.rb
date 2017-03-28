85b848028fadf352da73293357c7aae30ebd03d2
panyujin <panyujinyx@gmail.com>
2017-03-28 20:24:13 +0800

    implement movie relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
 
   has_many :movies
   has_many :reviews
+  has_many :movie_relationships
+  has_many :participated_movies, :through => :movie_relationships, :source => :group
 end
