fcebd45fc52166d733ca3b90fcb7e4611c31af80
panyujin <panyujinyx@gmail.com>
2017-03-28 23:49:51 +0800

    implement my movies

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  namespace :account do
+    resources :movies
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#index'
 end
